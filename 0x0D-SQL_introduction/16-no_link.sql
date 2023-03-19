c768a4435e88b99e02ef3d563cd3a5ac7ecee403
JoyEmma12 <ejoy0600@gmail.com>
2023-03-19 02:49:46 -0700

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- Scripts that lists all records of second_table in MYSQL
 
-SELECT name, score FROM second_table WHERE name IS NOT NULL ORDER BY score DESC; 
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC; 
