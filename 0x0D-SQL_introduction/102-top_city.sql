d292ac96e0b12f583cc529e6ee7d5c1f54a82f82
JoyEmma12 <ejoy0600@gmail.com>
2023-03-19 03:21:04 -0700

    task 19

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,3 @@
+-- Script that displays top 3 cities temperatur in MYSQL
+
+SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month = '7' OR month = '8' GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
