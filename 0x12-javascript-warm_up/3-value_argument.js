72e30027bf001163902204f97b8000ef17a79c17
JoyEmma12 <ejoy0600@gmail.com>
2023-04-13 01:03:12 +0100

    task3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-if (process.argv[2]) {
-	console.log(process.argv[2]);
+if (process.argv[2])
+{
+	 console.log(process.argv[2]);
 } else {
-	console.log('No arguement');
+	 console.log('No arguement');
 }
