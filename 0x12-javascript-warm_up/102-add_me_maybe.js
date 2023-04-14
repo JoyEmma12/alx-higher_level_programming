59e9b0b80b553bc7db8ed8770508a0f2c5048ee4
JoyEmma12 <ejoy0600@gmail.com>
2023-04-14 14:56:35 +0100

    task 16

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 exports.addMeMaybe = function (number, theFunction) {
-  for (let i = 0; i < number; ++i) {
-    thefunction(number);
-  }
+  ++number;
+  theFunction(number);
 };
