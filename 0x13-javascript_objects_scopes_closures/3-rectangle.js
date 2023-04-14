fc1843a7c9b21b3423c4018ea170acb20bc08399
JoyEmma12 <ejoy0600@gmail.com>
2023-04-14 20:06:10 +0100

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+   constructor (w, h) {
+	   if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0)  {
+		   this.width = w;
+		   this.width = h;
+	   }
+   }
+
+
+print () {
+	for (let i = 0; i < this.height; ++i)  {
+		let j = 0;
+
+		while (j < this.width) {
+			++j;
+			process.stdout.write('X');
+		}
+
+		if (j === this.width)  {
+			console.log('');
+		}
+	}
+}
+};
