aeb22c12d1caf48a801e098922add1707886e3fe
JoyEmma12 <ejoy0600@gmail.com>
2023-04-28 11:45:58 -0700

    task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a JSON POST resquest to aURL passed as the first arguement and displays the body
-curl -s -X POST -H "Contnet-Type: application/json" --data-binary "@$2" "$1"
+curl -s -H POST -H "Content-Type: application/json" -d  "$(cat "$2")" "$1"
