8bafc451ac2e068657932efd96ce9b35d3dc575b
JoyEmma12 <ejoy0600@gmail.com>
2023-04-28 10:09:50 -0700

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a GET request and displays the body of thw response.
-url="$1"
-response=$(curl -sSL -w "%{status_code}" $url)
-if [ "$response" == "200" ]; then
-   echo "$(curl -sSL $url)"
-else
-   echo "Error: Response code was $response"
-fi
+# Script that takes in a URL, sends a GET request and displays the body of the response.
+curl -sL "$1"
