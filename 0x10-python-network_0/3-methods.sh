7d2ac97202916b7175b431a168fed37f37e127f1
JoyEmma12 <ejoy0600@gmail.com>
2023-04-28 10:41:26 -0700

    task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Script that takes in URL and display all HTTPS methods
-url="$1"
-
-response=$(curl -sSl -X OPTIONS $url)
-	methods=$(echo "$response" | grep -i "allow" | sed -e 's/Allow: //i')
-	echo "methods for $url are: $methods" 
+# Script that takes in URL and display all HTTPS methods.
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
