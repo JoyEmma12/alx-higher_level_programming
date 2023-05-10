38868162a84d1220bbc48920333d44c120790d73
JoyEmma12 <ejoy0600@gmail.com>
2023-05-10 13:46:49 -0700

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that takes URL and an email, 
+Sends a POST request to the passed URL and 
+Displays the body of the response in utf-8
+"""
+
+import urllib.request
+import sys
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(email)
+    data = data.encode("utf-8")
+    request_url = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request_url) as response:
+        print(response.read().decode("utf-8"))
