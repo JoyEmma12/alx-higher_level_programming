09e082257877b97c5eb21b9e23b5df69309dceec
JoyEmma12 <ejoy0600@gmail.com>
2023-05-10 08:40:59 -0700

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Script that fetches url
+
+import urllib.request
+
+
+if __name__ == "__main__":
+	request_url = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+	with urllib.request.urlopen(request_url) as response:
+		the_page = response.read()
+		print("Body response:")
+		print("\t- type: {}".format(type(the_page)))
+		print("\t- content: {}".format(the_page))
+		print("\t- utf8 content: {}".format(the_page.decode("utf-8")))
