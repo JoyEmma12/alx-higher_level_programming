d0eefbac54d58503b3d52d0b803e946fddb11ab0
JoyEmma12 <ejoy0600@gmail.com>
2023-05-10 13:49:18 -0700

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Script that takes URL and an email, 
-Sends a POST request to the passed URL and 
+Script that takes URL and an email,
+Sends a POST request to the passed URL and
 Displays the body of the response in utf-8
 """
 
