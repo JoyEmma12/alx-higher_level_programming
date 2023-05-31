6b5de9ffcfdd6ccc1033605ef5fce9430a436024
JoyEmma12 <ejoy0600@gmail.com>
2023-05-31 22:55:47 +0100

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const fs = require('fs');
 arg1 = process.argv[2];
 
 fs.get(arg1).on('response', (response) => {
-  console.log("code: ${response.statusCode}");
+  console.log(`code: ${response.statusCode}`);
 });
