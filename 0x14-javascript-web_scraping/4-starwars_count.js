b452bf491a5721628965967c190f460f667f816d
JoyEmma12 <ejoy0600@gmail.com>
2023-06-01 11:49:50 +0100

    commit

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-
-const request = require("request");
-
-const url = process.argv[2];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
-  const movie = JSON.parse(body).results;
-  const wedge = movie.filter((movie) =>
-    movie.characters.includes("https://swapi-api.alx-tools.com/api/people/14/")
-  );
-  console.log(wedge.length);
 });
